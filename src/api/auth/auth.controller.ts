f9194090cc27802f5cd8e22da05663776ac500a7
HunterTigerX <HunterTigerX@gmail.com>
2024-03-27 22:28:35 +0300

    feat: added filters to auth controller

--- src/api/auth/auth.controller.ts
+++ src/api/auth/auth.controller.ts
@@ -1,9 +1,10 @@
-import { Controller, Post, Body, UseGuards } from '@nestjs/common';
+import { Controller, Post, Body, UseGuards, UseFilters } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto, validateRefreshToken } from './validation/signupDTO';
 import { IRefreshToken } from './interface/auth.interfaces';
 import { RefreshTokenGuard } from '../guards/tokensGuards';
 import { LoggingService } from '../logger/logger.service';
+import { CustomExceptionFilter } from '../filter/exception-filter.service';
 
 @Controller('auth')
 export class AuthController {
@@ -12,17 +13,20 @@ export class AuthController {
     private readonly logger: LoggingService,
   ) {}
 
+  @UseFilters(CustomExceptionFilter)
   @Post('/signup')
   signUserUp(@Body() createUserDto: CreateUserDto) {
     return this.authService.signUser(createUserDto);
   }
 
+  @UseFilters(CustomExceptionFilter)
   @Post('/login')
   logUserIn(@Body() loginUserDto: CreateUserDto) {
     return this.authService.logUser(loginUserDto);
   }
 
   @UseGuards(RefreshTokenGuard)
+  @UseFilters(CustomExceptionFilter)
   @Post('/refresh')
   refreshToken(@Body() refreshTokenDto: IRefreshToken) {
     validateRefreshToken(refreshTokenDto);
